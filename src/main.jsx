e666f4131797703eb7ecf3d9051f1ed99df725d5
ZiDinchick <fredy.04@mail.ru>
2024-11-01 14:46:27 +0700

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Footer } from "./components/Footer.jsx";
 import { Header } from "./components/Header.jsx";
-import { Shop } from "./components/Shop.jsx";
+import { Shop } from "./components/shop.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
